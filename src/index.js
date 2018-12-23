c99016c513a0024e5e3f049122e87bb4c3b5fd2e
Artur Grg <sato.alta@gmail.com>
2018-12-23 18:49:53 -0500

    Update our store and test it.

--- src/index.js
+++ src/index.js
@@ -7,14 +7,12 @@ import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 
 function reducer(state, action) {
-  console.log(action);
-
-  return "Store";
+  if (action.type === "changeState") {
+    return action.payload.newState;
+  }
 }
 
 const store = createStore(reducer);
-
-console.log(store.getState());
 
 const action = {
   type: "changeState",
@@ -24,6 +22,8 @@ const action = {
 };
 
 store.dispatch(action);
+
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
